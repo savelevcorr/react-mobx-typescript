0dcef5c4ea0d13be2f3fc3e32da347745da25af6
Andrew Savelev <savelevcorr@gmail.com>
2020-03-31 17:06:48 +0300

    Re-render view

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {autorun} from "mobx";
+
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import store from './stores';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App stories={store.storyStore.readableStories}
-             onArchiveHandler={(id) => {store.archiveStore.archiveStoryIds.push(id) }}/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+function render () {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App stories={store.storyStore.readableStories}
+                 onArchiveHandler={(id) => {store.archiveStore.archiveStoryIds.push(id) }}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+autorun(render);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
